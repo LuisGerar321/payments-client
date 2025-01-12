de89c596e915c3a01b0bf6b10f0813811ef5bd4b
luisgerar321 <dojas64@gmail.com>
2025-01-12 08:13:54 -0600

    menu refactor, sections maps to list items

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Box, Grid } from "@mui/material";
 import { config } from "./config";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Payments } from "./components/payments/Layout";
 import { MenuLayout } from "./components/menu/Layout";
 import { TransactionLayout } from "./components/transactions/Layout";
@@ -13,32 +13,43 @@ function App() {
 
   useEffect(() => {}, [token]);
   return (
-    <Box
-      sx={{
-        m: 0,
-        p: 0,
-        backgroundColor: palleteColor.background,
-        backgroundImage: !token ? `linear-gradient(to bottom, ${palleteColor.alternativeGradient.map((color) => color).join(", ")})` : undefined,
-        width: "100%",
-        height: "100vh",
-        display: "flex",
-        overflowY: { xs: "auto", md: "hidden" },
-      }}
-    >
-      {token ? (
-        <Grid container>
-          <Grid item xs={12} md={2}>
-            <MenuLayout></MenuLayout>
+    <Router>
+      <Box
+        sx={{
+          m: 0,
+          p: 0,
+          backgroundColor: palleteColor.background,
+          backgroundImage: !token ? `linear-gradient(to bottom, ${palleteColor.alternativeGradient.map((color) => color).join(", ")})` : undefined,
+          width: "100%",
+          height: "100vh",
+          display: "flex",
+          overflowY: { xs: "auto", md: "hidden" },
+        }}
+      >
+        {token ? (
+          <Grid container>
+            <Grid item xs={12} md={2}>
+              <MenuLayout></MenuLayout>
+            </Grid>
+            <Grid item xs={12} md={10}>
+              <Routes>
+                <Route
+                  path="/payments"
+                  element={
+                    <>
+                      <Payments />
+                      <TransactionLayout />
+                    </>
+                  }
+                />
+              </Routes>
+            </Grid>
           </Grid>
-          <Grid item xs={12} md={10}>
-            <Payments></Payments>
-            <TransactionLayout></TransactionLayout>
-          </Grid>
-        </Grid>
-      ) : (
-        <AuthLayout></AuthLayout>
-      )}
-    </Box>
+        ) : (
+          <AuthLayout></AuthLayout>
+        )}
+      </Box>
+    </Router>
   );
 }
 
--- src/components/menu/Slider.tsx
+++ src/components/menu/Slider.tsx
@@ -1,61 +1,97 @@
-import { FC, useState } from "react";
+import { FC, useMemo, useState, cloneElement } from "react";
 import { IProps } from "../../interfaces";
 import { config } from "../../config";
 import { Avatar, Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
 import MoneyIcon from "@mui/icons-material/CurrencyExchange";
 import ReportsIcon from "@mui/icons-material/Assessment";
+import { Category, Dashboard, Liquor } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
 
 interface IDrawerProps extends IProps {
   balance: number;
 }
 
 const { palleteColor } = config;
+const sections = [
+  {
+    name: "Dashboard",
+    description: null,
+    icon: <Dashboard></Dashboard>,
+    pathRoute: "/dashboard",
+  },
+  {
+    name: "Categories",
+    description: null,
+    icon: <Category></Category>,
+    pathRoute: "/categories",
+  },
+  {
+    name: "Products",
+    description: null,
+    icon: <Liquor></Liquor>,
+    pathRoute: "/products",
+  },
+  {
+    name: "Payments",
+    description: null,
+    icon: <MoneyIcon> </MoneyIcon>,
+    pathRoute: "/payments",
+  },
+  {
+    name: "Reports",
+    description: null,
+    icon: <ReportsIcon> </ReportsIcon>,
+    pathRoute: "/reports",
+  },
+];
 
 export const PersistentSlider: FC<IDrawerProps> = (props: IDrawerProps) => {
   const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
+  const navigate = useNavigate();
 
-  const handleListItemClick = (index: number) => {
+  const handleListItemClick = (index: number, pathRoute: string) => {
     setSelectedIndex(index);
+    navigate(pathRoute);
   };
+
+  const paymentsIndex = useMemo(() => {
+    return sections.findIndex((section) => section.name === "Payments");
+  }, [sections]);
 
   return (
     <Box sx={{ display: "flex", alignItems: "center" }}>
       <List>
-        <ListItem>
-          <ListItemButton onClick={() => handleListItemClick(0)}>
-            <ListItemIcon>
-              <Avatar
-                variant="rounded"
-                sx={{
-                  backgroundColor: selectedIndex === 0 ? palleteColor.primary : "white",
+        {sections.map((section, index) => (
+          <ListItem>
+            <ListItemButton
+              sx={{
+                "&:hover": {
                   borderRadius: 3,
-                  boxShadow: `0px 4px 10px rgba(0,0,0,0.2)`,
-                }}
-              >
-                <MoneyIcon sx={{ color: selectedIndex === 0 ? "white" : palleteColor.secondary }} />
-              </Avatar>
-            </ListItemIcon>
-            <ListItemText sx={{ color: palleteColor.dark }} primary="Payments" secondary={`$${props.balance || 0}`} />
-          </ListItemButton>
-        </ListItem>
-
-        <ListItem>
-          <ListItemButton onClick={() => handleListItemClick(1)}>
-            <ListItemIcon>
-              <Avatar
-                variant="rounded"
-                sx={{
-                  backgroundColor: selectedIndex === 1 ? palleteColor.primary : "white",
-                  borderRadius: 3,
-                  boxShadow: `0px 4px 10px rgba(0,0,0,0.2)`,
-                }}
-              >
-                <ReportsIcon sx={{ color: selectedIndex === 1 ? "white" : palleteColor.secondary }} />
-              </Avatar>
-            </ListItemIcon>
-            <ListItemText primary="Reports" />
-          </ListItemButton>
-        </ListItem>
+                  backgroundColor: palleteColor.primaryGradient,
+                },
+              }}
+              onClick={() => handleListItemClick(index, section.pathRoute)}
+            >
+              <ListItemIcon>
+                <Avatar
+                  variant="rounded"
+                  sx={{
+                    backgroundColor: selectedIndex === index ? palleteColor.primary : "white",
+                    borderRadius: 3,
+                    boxShadow: `0px 4px 10px rgba(0,0,0,0.2)`,
+                  }}
+                >
+                  {cloneElement(section.icon, {
+                    sx: {
+                      color: selectedIndex === index ? "white" : palleteColor.secondary,
+                    },
+                  })}
+                </Avatar>
+              </ListItemIcon>
+              <ListItemText sx={{ color: palleteColor.dark }} primary={section.name} secondary={index === paymentsIndex ? `$${props.balance}` : section.description} />
+            </ListItemButton>
+          </ListItem>
+        ))}
       </List>
     </Box>
   );
